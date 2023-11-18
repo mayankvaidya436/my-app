f8a3664c55767a31fa66e55acb2709df602e0d6b
mayankvaidya436 <mayankvaidya436@gmail.com>
2023-11-18 13:31:47 +0530

    expensenitem

---
+++ src/components/Expenseitem.js
@@ -0,0 +1,15 @@
+function ExpenseItem()
+{
+    return (<div>
+        <div>expense items</div>
+        <h3> food rs 10 </h3>
+        <h3>Petrol Rs 100</h3>
+
+          <h3>Movies Rs 200</h3>
+
+    </div>)
+
+        
+    
+}
+export default ExpenseItem;
