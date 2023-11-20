d93d00093c972855dd638f11c07a472c54f1e812
mayankvaidya436 <mayankvaidya436@gmail.com>
2023-11-20 17:14:40 +0530

    add button

--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -5,12 +5,15 @@ import './Expenseitem.css';
 
 const ExpenseItem=(props)=>
 {   
+     const del=()=>{
+         console.log("delete");
+     }
      return (<div className="expense-item">
            <ExpenseDate date={props.date}/>
         <div  className="expense-item__desc">
            <ExpenseDetials title={props.title} location={props.location}
            amount={props.amount}/>
-           
+           <button onClick={del}>Delete expense</button>
         </div>
 
     </div>)
